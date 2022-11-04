2747641d494ec9f75c55bee075c92b47d6997aa7
ChadOW <chad.estoup@gmail.com>
2022-11-04 22:33:23 +0100

    QRCode generator

---
+++ qrc.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Desc: QR Code generator for links
+# requirements: qrencode
+# Author: Chad Estoup--Streiff 
+
+# Check if qrencode exists
+if ! command -v qrencode &> /dev/null
+then
+    echo "qrencode could not be found"
+    exit
+fi
+
+# Check if url is set
+if [ $# -eq 0 ]
+then
+    echo "Precise a url"
+    exit 1
+fi
+
+qrencode -o qrcode.png $1
